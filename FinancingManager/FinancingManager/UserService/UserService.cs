8863a9aa236c85e1ec3438d850a63584b1a0d9c5
Andriy Rokunets <andrejrokunec@gmail.com>
2024-05-15 17:18:30 +0300

    Add user service

---
+++ FinancingManager/FinancingManager/UserService/UserService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancingManager.UserService
+{
+    public class UserService
+    {
+        private readonly UserRepository.UserRepository userRepository;
+
+        public UserService(UserRepository.UserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public bool Login(string userName, string password)
+        {
+            var user = userRepository.GetAll().FirstOrDefault(user => user.UserName == userName);
+            if (user == null)
+            {
+                return false;
+            }
+            if (user.Password == password) 
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
