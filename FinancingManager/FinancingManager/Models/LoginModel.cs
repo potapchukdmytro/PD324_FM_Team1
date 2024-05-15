7e12224d59bc41609879425c18f94f88b7ef272a
Dmytro Potapchuk <traiget@gmail.com>
2024-05-15 16:09:50 +0300

    Add login model

---
+++ FinancingManager/FinancingManager/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FinancingManager.Models
+{
+    public class LoginModel
+    {
+        public required string Login { get; set; }
+        public required string Password { get; set; }
+    }
+}
