fd27340bece2c754be9d7304ba24914f75318fd3
Dmytro Potapchuk <traiget@gmail.com>
2024-05-17 15:56:15 +0300

    add cost repository

---
+++ FinancingManager/FinancingManager/Repositories/CostRepository.cs
@@ -0,0 +1,59 @@
+﻿using FinancingManager.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinancingManager.Repositories
+{
+    public class CostRepository
+    {
+        private readonly AppDbContext context;
+
+        public CostRepository(AppDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public void Add(CostEntity entity)
+        {
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public async Task AddAsync(CostEntity entity)
+        {
+            context.Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<CostEntity?> GetById(int id)
+        {
+            return await context.Costs.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            var entity = await GetById(id);
+
+            if (entity != null)
+            {
+                await RemoveAsync(entity);
+            }
+        }
+
+        public async Task RemoveAsync(CostEntity entity)
+        {
+            context.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(CostEntity entity)
+        {
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public IQueryable<CostEntity> GetAll()
+        {
+            return context.Costs.AsNoTracking().Include(c => c.User);
+        }
+    }
+}
---
+++ FinancingManager/FinancingManager/Services/CostService.cs
@@ -0,0 +1,14 @@
+﻿using FinancingManager.Repositories;
+
+namespace FinancingManager.Services
+{
+    public class CostService
+    {
+        private readonly CostRepository costRepository;
+
+        public CostService(CostRepository costRepository)
+        {
+            this.costRepository = costRepository;
+        }
+    }
+}
