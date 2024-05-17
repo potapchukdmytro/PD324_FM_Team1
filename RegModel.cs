5f2516de028a51a0402a70f1caa394350a1ab63c
IllyaHim <illahim120@gmail.com>
2024-05-17 15:50:05 +0300

    Add files via upload

---
+++ RegModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class RegistrationModel
+{
+	public RegistrationModel()
+	{
+		public required string userName { get; set; }
+	    public required string password { get; set; }
+	    public required string email { get; set; }
+	}
+}
