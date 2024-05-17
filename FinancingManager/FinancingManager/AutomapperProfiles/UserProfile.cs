16fb9b655abfbc4c5809129309f34cee55c5d288
1ALAR1 <senchuk.yuriy85@gmail.com>
2024-05-17 16:47:44 +0300

    add register model and automapper profile

---
+++ FinancingManager/FinancingManager/AutomapperProfiles/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using FinancingManager.Entities;
+using FinancingManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancingManager.AutomapperProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<RegisterModel, UserEntity>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.FirstName));
+        }
+    }
+}
